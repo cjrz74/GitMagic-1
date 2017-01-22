de92f69bf20afdba9e1efc0319c6a53682116d14
wzaztl <wzaztl@163.com>
2017-01-22 17:18:29 -0600

    Create xlfgb.c

---
+++ sentences/xlfgb.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int xlfgbSentence1(){
+	printf("This is a really interesting assignment.\n");
+	return 1;
+}
+
+int xlfgbSentence2(){
+	printf("I wonder if done the assignment correctly?\n");
+	return 1;
+}
