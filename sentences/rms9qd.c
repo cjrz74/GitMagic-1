4f2614f20389f75df7457763edc841ae5e4c63cc
Ryon Siebel <rmsiebel@me.com>
2017-01-22 22:13:14 -0600

    Added two sentences.

---
+++ sentences/rms9qd.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void rms9qd_print1() {
+	printf(" Blessed Jagex will surely nuke those filthy bots!");
+}
+
+void rms9qd_print2() {
+	printf(" My name is Zezima, king of all of Rune Scpae!");
+}
