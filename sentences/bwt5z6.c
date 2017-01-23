082c9389e6e8cfe3a6e65347364fb2a2f00e4f37
bwt5z6 <bwt5z6@mail.missouri.edu>
2017-01-23 16:29:24 -0600

    Add files via upload

---
+++ sentences/bwt5z6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int bwt5z6Sentence1()
+{
+	printf(". But he only had a limited supply of Twinkies.");
+	return 1;
+}
+
+int bwt5z6Sentence2()
+{
+	printf("So he went to a differnt store to buy some more.");
+	return 1;
+}
