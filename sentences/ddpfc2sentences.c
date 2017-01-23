f6ad386737c6e17a1330c06616cc47a6230a5203
pevahousedavid <ddpfc2@mail.missouri.edu>
2017-01-23 15:15:03 -0600

    Create ddpfc2sentences.c

---
+++ sentences/ddpfc2sentences.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int ddpfc2Sentence1()
+{
+	printf(“Who was not really a young man, but still”);
+	return 1;
+}
+
+int ddpfc2Sentence2()
+{
+	printf(“After he had his fill, he began to scream frantically”);
+	return 1;
+}
