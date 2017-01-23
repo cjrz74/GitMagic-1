0d8959066257e3eff537d485aa1b36e73ecd32f8
ldsz89 <ldsz89@mail.missouri.edu>
2017-01-22 18:43:47 -0600

    ldsz89-revise
    Wanted to change me sentences.

--- sentences/ldsz89.c
+++ sentences/ldsz89.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int ldsz89Sentence1() {
-    printf("This is what it feels like to be schizophrenic.");
+    printf("The bear the landed on top of me.");
     return 1;
 }
 
 int ldsz89Sentence2() {
-    printf("Also, I still play Runescape and I'm 21.");
+    printf("The diameter of a squid's heart is not as big as its eye.");
     return 1;
 }
 
