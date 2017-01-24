9afb3fa10025a5e1c6e61eb98fb070912dfa2439
kac2z7 <kac2z7@mail.missouri.edu>
2017-01-23 20:27:46 -0600

    Create kac2z7
    Creating my sentence .c file

---
+++ sentences/kac2z7.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+
+int kac2z7Sentence1()
+{
+  printf("He at so much he became full.");
+  return 1;
+}
+
+int kac2z7Sentence2()
+{
+  printf("After that he decided he needed to do his homework.");
+  return 1;
+}
