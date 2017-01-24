8b51e57cedd8fef23b20163475ea63005a0f97db
ChrisHarling <cehxc5@mail.missouri.edu>
2017-01-23 19:08:00 -0600

    Create cehxc5.c

---
+++ sentences/cehxc5.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+
+int cehxc5First()
+{
+  printf(" He decided he wanted to play a video game. ");
+  return 1;
+}
+
+int cehxc5Second()
+{
+  printf(" Twinkies are overated, snickers are way better");
+  return 1;
+}
